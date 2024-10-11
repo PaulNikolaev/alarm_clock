b8f2c55aa46c64aef8188950e0aeee19298ab7e9
Pavel Nikolaev <panikolaev7@gmail.com>
2024-10-11 18:46:34 +0300

    Add set_reminder

--- alarm_clock.py
+++ alarm_clock.py
@@ -6,13 +6,26 @@ import time
 import pygame
 
 
+def set_reminder():
+    rem = sd.askstring('Время напоминания', 'Введите время в формате: чч:мм (в 24-часовом формате)').split(':')
+    if rem:
+        try:
+            hour, minute = int(rem[0]), int(rem[1])
+            now = datetime.datetime.now()
+            dt = now.replace(hour=hour, minute=minute)
+            t = dt.timestamp()
+        except Exception as e:
+            mb.showerror('Ошибка!', f'Произошла ошибка: {e}!')
+
+
+
 window = Tk()
 window.title('Будильник')
 window.config(bg='white')
 
 label = Label(window, text='Напоминание', font=('Arial', 16), bg='white')
 label.pack(pady=10)
-set_button = Button(window, text='Установить напоминание', font=('Arial', 14), command=set)
+set_button = Button(window, text='Установить напоминание', font=('Arial', 14), command=set_reminder)
 set_button.pack(padx=10, pady=(0, 10))
 
 window.mainloop()
