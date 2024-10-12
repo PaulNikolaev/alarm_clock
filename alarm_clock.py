1320fee392be32d304313b0796f3593f4f804d65
Pavel Nikolaev <panikolaev7@gmail.com>
2024-10-12 07:05:09 +0300

    Add exit_window

--- alarm_clock.py
+++ alarm_clock.py
@@ -7,7 +7,7 @@ import pygame
 
 
 def set_reminder():
-    global t
+    global t, text
     rem = sd.askstring('Время напоминания',
                        'Введите время в формате: чч:мм (в 24-часовом формате)').split(':')
     if rem:
@@ -16,17 +16,22 @@ def set_reminder():
             now = datetime.datetime.now()
             dt = now.replace(hour=hour, minute=minute, second=0)
             t = dt.timestamp()
+            text = sd.askstring('Текст напоминания', 'Введите текст напоминания.')
+            label.config(text=f"Напоминание сработает\n"
+                              f"в {hour:02} часов {minute:02} мин.\n Текст напоминания:\n {text}.")
         except Exception as e:
             mb.showerror('Ошибка!', f'Произошла ошибка: {e}!')
 
 
 def check():
-    global t
+    global t, text
     if t:
         now = time.time()
         if now >= t:
             play_snd()
+            mb.showinfo('Напоминание', f'Пора {text}!')
             t = None
+            text = None
     window.after(10000, check)
 
 
@@ -46,8 +51,13 @@ def stop_music():
     label.config(text='Установить новое напоминание.')
 
 
-t = 0
+def exit_window():
+    window.destroy()
+
+
+t = None
 music = False
+text = None
 window = Tk()
 window.title('Будильник')
 window.config(bg='white')
@@ -59,6 +69,9 @@ set_button.pack(padx=10, pady=(0, 10))
 
 stop_button = Button(window, text='Остановить музыку', width=25, font=('Arial', 14), command=stop_music)
 stop_button.pack(padx=10, pady=(0, 10))
+
+exit_button = Button(window, text='Выход', width=25, font=('Arial', 14), command=exit_window)
+exit_button.pack(padx=10, pady=(0, 10))
 
 check()
 
